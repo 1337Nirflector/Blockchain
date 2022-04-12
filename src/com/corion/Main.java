2b130861a095200ade502746411c79acb0ab1b73
Nirflector <nirflector@gmail.com>
2022-04-12 19:55:53 +0200

    Add Test case

--- src/com/corion/Main.java
+++ src/com/corion/Main.java
@@ -3,6 +3,12 @@ package com.corion;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        BlockUtils blockUtils = new BlockUtils();
+        blockUtils.addBlock("500");
+        blockUtils.addBlock("600");
+        blockUtils.addBlock("1000");
+        System.out.println(blockUtils.isChainValid());
+        blockUtils.getBlockChain().get(2).setData("8000");
+        System.out.println(blockUtils.isChainValid());
     }
 }
